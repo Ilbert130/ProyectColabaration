5c1c653cb0fcee24c383f38f31ffd8e8a2fa3cbd
Ilbert130 <ilbertca27@gmail.com>
2022-03-19 00:18:48 +0800

    Agregamos los DTOs de Grupo

---
+++ ProyectPractices/DTOs/GrupoGetListDTO.cs
@@ -0,0 +1,16 @@
+﻿using ProyectPractices.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProyectPractices.DTOs
+{
+    public class GrupoGetListDTO
+    {
+        [Required]
+        public int Id { get; set; }
+        [StringLength(maximumLength: 5)]
+        public string Nombre { get; set; }
+        [Required]
+        public int MaestroId { get; set; }
+        public List<Alumno> Alumnos { get; set; }
+    }
+}
---
+++ ProyectPractices/DTOs/GrupoPutDTO.cs
@@ -0,0 +1,15 @@
+﻿using ProyectPractices.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProyectPractices.DTOs
+{
+    public class GrupoPutDTO
+    {
+        [Required]
+        public int Id { get; set; }
+        [StringLength(maximumLength: 5)]
+        public string Nombre { get; set; }
+        [Required]
+        public int MaestroId { get; set; }
+    }
+}
